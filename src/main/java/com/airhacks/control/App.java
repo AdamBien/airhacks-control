1b26e515ed570907aa04a52de9a12f3912e6aa25
Adam Bien <abien@adam-bien.com>
2013-04-05 19:10:13 +0200

    Title fixed

--- src/main/java/com/airhacks/control/App.java
+++ src/main/java/com/airhacks/control/App.java
@@ -12,24 +12,24 @@ import javafx.stage.Stage;
  * @author adam-bien.com
  */
 public class App extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         AirhacksView appView = new AirhacksView();
         Scene scene = new Scene(appView.getView());
-        stage.setTitle("AirhHacks Control");
+        stage.setTitle("AirHacks Control");
         final String uri = getClass().getResource("app.css").toExternalForm();
         scene.getStylesheets().add(uri);
         stage.setScene(scene);
         stage.show();
     }
-    
+
     @Override
     public void stop() throws Exception {
         RegistrationService service = InjectorProvider.get().getInstance(RegistrationService.class);
         service.close();
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
