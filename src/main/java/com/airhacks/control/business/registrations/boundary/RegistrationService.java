7644ddb2ea760e434706fb69ae078580c7f81a3a
Ryan Michela <rmichela@twilio.com>
2023-02-03 15:11:16 -0500

    Upgrade to Java17 with updated dependencies

--- src/main/java/com/airhacks/control/business/registrations/boundary/RegistrationService.java
+++ src/main/java/com/airhacks/control/business/registrations/boundary/RegistrationService.java
@@ -3,10 +3,10 @@ package com.airhacks.control.business.registrations.boundary;
 import com.airhacks.control.business.registrations.entity.Attendee;
 import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
 /**
  *
